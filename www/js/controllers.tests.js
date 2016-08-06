00217153ec8e88b7c305cf194da928987ec6b221
jay <jay3dec@gmail.com>
2016-08-06 15:34:48 +0530

    frist

---
+++ www/js/controllers.tests.js
@@ -0,0 +1,20 @@
+describe('Controllers', function(){
+    var scope;
+
+    // load the controller's module
+    beforeEach(module('starter.controllers'));
+
+    beforeEach(inject(function($rootScope, $controller) {
+        scope = $rootScope.$new();
+        $controller('AccountCtrl', {$scope: scope});
+    }));
+
+    // tests start here
+    it('should have enabled friends to be true', function(){
+        expect(scope.settings.enableFriends).toEqual(true);
+    });
+
+    it('should have enabled boy to be true',function(){
+    	expect(scope.settings.enableBoy).toEqual(true);
+    });
+});
